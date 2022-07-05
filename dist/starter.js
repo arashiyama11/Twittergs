c51a49ca8114610547724d5e2422c0023a7353c3
jinjanow <arashiyama1120@gmail.com>
2022-07-05 18:56:44 +0900

    satrter.jsの追加 (close #7)

---
+++ dist/starter.js
@@ -0,0 +1,26 @@
+function setEnv(){
+  PropertiesService.getUserProperties().setProperties({
+    CLIENT_ID:"<CLIENT_ID>",
+    CLIENT_SECRET:"<CLIENT_SECRET>",
+    API_KEY:"<API_KEY>",
+    API_SECRET:"<API_SECRET>",
+    BEARER_TOKEN:"<BEARER_TOKEN>"
+  })
+}
+
+function authorize(){
+  const client=new Client({
+    name:"@auto",
+    oauthVersion:"1.0a"
+  })
+  Logger.log(client.authorize())
+}
+
+function authCallBack(e){
+  const result=Client.fromCallBackEvent({e:e}).isAuthorized(e)
+  if(result){
+    return HtmlService.createHtmlOutput("成功")
+  }else{
+    return HtmlService.createHtmlOutput("失敗")
+  }
+}
